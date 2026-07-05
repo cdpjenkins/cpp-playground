bc465f8290ce69806e222253fb1e7d5ad580938c
Chris Jenkins <cdpjenkins@gmail.com>
2026-07-05 14:17:47 +0100

    feat: supposedly faster implemention using bit stuffing from a LinkedIn post

--- playground/src/array_shuffle.cpp
+++ playground/src/array_shuffle.cpp
@@ -4,10 +4,11 @@
 
 #include "array_shuffle.h"
 
-#import <vector>
+#include <vector>
+#include <bit>
 using namespace std;
 
-std::vector<int> array_shuffle(vector<int>& array, int n)
+vector<int> naive_shuffle(vector<int>& array, int n)
 {
     vector<int> result{};
 
@@ -16,6 +17,35 @@ std::vector<int> array_shuffle(vector<int>& array, int n)
         result.push_back(array[i]);
         result.push_back(array[i + n]);
     }
+    return result;
+}
+
+// Solution from https://www.linkedin.com/feed/update/activity:7478882097231777792/
+vector<int> fast_shuffle(vector<int>& array, int n)
+{
+    constexpr unsigned int max_value = 1000;
+    constexpr int bit_width = std::bit_width(max_value);
+    constexpr int mask = (1 << bit_width) - 1;
+
+    for (int i = 0, j = 0; i < n; i++, j += 2)
+    {
+        array[j] |= (array[i] & mask) << bit_width;
+        array[j + 1] |= (array[i + n] & mask) << bit_width;
+    }
+
+    for (int i = 0; i < n*2; i++)
+    {
+        array[i] >>= bit_width;
+    }
+
+    // given we're copying the vector at the end, it's not clear that this is any more efficient than the naive version
+    return array;
+}
+
+
+std::vector<int> array_shuffle(vector<int>& array, int n)
+{
+    vector<int> result = fast_shuffle(array, n);
 
     return result;
 }
